87f6fbae0bf52a3b1e99ebc8ee655a8da2600b4d
Aneela Noreen <aneela.uet@gmail.com>
2018-01-20 03:21:53 +0500

    Commit Code of URL changes

--- app/handler.js
+++ app/handler.js
@@ -1,55 +1,61 @@
 'use strict';
-var multipart = require("./common/parser");
 var Dropbox = require('dropbox');
-var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var bl = require('bl');
 
 module.exports.upload = (event, context, callback) => {
-  
+  console.log(JSON.stringify(event));
+
+  var fileUrl = '';
   var dropboxPath = '';
   var dropboxAccessCode = '';
   var filename = '';
   var formData = null;
+  var isHttps = false;
+  var msg;
 
-  var msg = '';
-
-  var sendResponse= function(status, msg) {
+  var sendResponse = function (status, msg) {
+    var body = JSON.stringify({
+      status: status,
+      message: msg
+    })
     var response = {
-      "status": status,
-      "message": msg
+      statusCode: 200,
+      body: body,
     };
     callback(null, response);
     return;
   };
 
-  try {
-
-    var bodyBuffer = new Buffer(event['body-json'].toString(), 'base64');
-    var boundary = multipart.getBoundary(event.params.header['content-type']);
-    var parts = multipart.Parse(bodyBuffer, boundary);
-
-    for (var i = 0; i < parts.length; i++) {
-      var part = parts[i];
-      console.log("part size:" + parts.length);
-      if (part.name === 'dropboxPath') {
-        dropboxPath = part.value;
-        while (dropboxPath.indexOf('\\') != -1) {
-          dropboxPath = dropboxPath.replace('\\', '/');
+  if (event['body'] != undefined) {
+    try {
+      var obj = JSON.parse(event['body']);
+      console.log("obj" + JSON.stringify(obj));
+      fileUrl = obj.fileUrl;
+      dropboxPath = obj.dropboxPath;
+      dropboxAccessCode = obj.dropboxAccessCode;
+      console.log("fileUrl:" + fileUrl);
+      if (fileUrl != undefined) {
+        if (fileUrl.indexOf("https") != -1) {
+          isHttps = true;
         }
-      } else if (part.name === 'dropboxAccessCode') {
-        dropboxAccessCode = part.value;
-      } else if (part.name === 'myfile') {
-        filename = part.filename;
-        formData = part.data;
+        filename = fileUrl.substr(fileUrl.lastIndexOf("/") + 1, fileUrl.length);
       }
+    } catch (err) {
+      msg = "Error:" + err;
+      console.log(msg);
+      sendResponse("error", msg);
     }
-  } catch (err) { 
-    msg = "Error while parsing multipaprt request.Error:" + err;
+  }
+
+  if (fileUrl == null || fileUrl == '') {
+    msg = "File URL is missing";
     console.log(msg);
     sendResponse("error", msg);
   }
-
-  if (dropboxAccessCode == null || dropboxAccessCode =='') {
-    msg = "dropboxAccessCode is missing" ;
+  if (dropboxAccessCode == null || dropboxAccessCode == '') {
+    msg = "dropboxAccessCode is missing";
     console.log(msg);
     sendResponse("error", msg);
   }
@@ -63,33 +69,33 @@ module.exports.upload = (event, context, callback) => {
     console.log(msg);
     sendResponse("error", msg);
   }
-  if (formData == null || formData == '') {
-    msg = "File Data is missing";
-    console.log(msg);
-    sendResponse("error", msg);
-  }
+
 
   console.log("uploading to drop box");
-  
+
+
   var dbx = new Dropbox({ accessToken: dropboxAccessCode });
+  var handler = (isHttps ? https : http);
 
-  var filePath = '';
+  var request = handler.get(fileUrl, function (response) {
+    response.pipe(bl(function (err, data) {
+      if (err)
+        return console.error(err)
+      var commitInfo = {
+        "contents": new Buffer(data),
+        "path": dropboxPath + filename
+      };
 
-  var commitInfo = {
-    "contents": formData,
-    "path": dropboxPath + filename
-  };
-  
-  dbx.filesUpload(commitInfo).then(function (response) {
-    msg = { response };
-    console.log("Upload file on dropbox successfully");
-    sendResponse("success", msg);
-  })
-    .catch(function (err) {
-      msg = err ;
-      console.log(msg);
-      sendResponse("error", msg);
-    });
-
-
+      dbx.filesUpload(commitInfo).then(function (response) {
+        msg = { response };
+        console.log("Upload file on dropbox successfully");
+        sendResponse("success", msg);
+      })
+        .catch(function (err) {
+          msg = err;
+          console.log(msg);
+          sendResponse("error", msg);
+        });
+    }))
+  });
 };
