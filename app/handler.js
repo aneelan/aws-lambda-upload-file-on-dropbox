54339345bdc2ccf165757f9f21941b76573c601e
Aneela Noreen <aneela.uet@gmail.com>
2018-01-19 05:50:28 +0500

    Code cleaning

--- app/handler.js
+++ app/handler.js
@@ -33,54 +33,32 @@ module.exports.upload = (event, context, callback) => {
     }
   }
   console.log("uploading to drop box");
-  console.log("Parts:"+JSON.stringify(parts));
+  
   var dbx = new Dropbox({ accessToken: dropboxAccessCode });
 
   var filePath = '';
 
-  //filePath = "/Users/aneela/Desktop/abc/" + filename;
-  /*filePath = filename;
-  fs.writeFile(filePath, formData, function (err) {
-    if (err) {
-      msg = "Error Writing file " + err;
+  var commitInfo = {
+    "contents": formData,
+    "path": dropboxPath + filename
+  };
+  //console.log("commitInfo:" + JSON.stringify(commitInfo));
+  dbx.filesUpload(commitInfo).then(function (response) {
+    msg = "Dropbox file upload successfully.Response: " + response;
+    console.log(msg);
+    context.succeed();
+    })
+    .catch(function (err) {
+      msg = "Error Uploading Dropbox file" + err;
       console.log(msg);
-      //context.fail("writeFile failed: " + err);
-    } else {
-      fs.readFile(filePath, function (err, data) {
-        if (err !== null) {
-          msg = "Error Reading file " + err;
-          console.log(msg);
-        }*/
-        var commitInfo = {
-          "contents": formData,
-          "path": dropboxPath + filename
-        };
-        //console.log("commitInfo:" + JSON.stringify(commitInfo));
-        dbx.filesUpload(commitInfo, function (error, stat) {
-          if (error !== null) {
-            msg = "Error Uploading Dropbox file" + error;
-            console.log(msg);
-          }
-          //context.succeed("writeFile succeeded");
-          msg = "Dropbox file upload successfully ";
-          console.log(msg);
+      context.fail();
+    });
+    
 
-          const response = {
-            statusCode: 200,
-            body: JSON.stringify({
-              message: msg,
-              input: event,
-            }),
-          };
+  const response = { "message": msg };
 
-          callback(null, response);
-        });
-//      });
-//    }
-//  });
+  callback(null, response);
   
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  
 };
 
